a41685e8f9584c163a7790ad3ba14d00cfc8402d
hitesh <hklenka2018@gmail.com>
2024-11-19 09:16:20 +0530

    Initial commit

---
+++ ProjectTracker/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.views import LogoutView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('', include('account.urls')),
+    path('projects/', include('project.urls')),
+    path('projects/<uuid:project_id>/', include('todolist.urls')),
+    path('projects/<uuid:project_id>/<uuid:todolist_id>/', include('task.urls')),
+    path('logout/', LogoutView.as_view(), name='logout'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
